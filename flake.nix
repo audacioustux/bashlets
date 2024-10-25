d9db6b5743ba144edfc06c75879b110967ab16c8
Tanjim Hossain <tangimhossain1@gmail.com>
2024-10-25 14:44:07 +0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
             version = "0.2";
             src = pkgs.fetchurl {
               url = "https://raw.githubusercontent.com/audacioustux/bashlets/main/bashlet.sh";
-              sha256 = "0rjn6657ywr8gfr6gynazkxmx7icqca44jjxwk8xm60i5p2gsv4a";
             };
             phases = [ "installPhase" ];
 
